d42f7f38887f6a6c53bf9260fe0c00441bf15643
Tom Rooker <tomdrooker@gmail.com>
2020-12-19 16:00:58 +0000

    images randomly assigned

--- script.js
+++ script.js
@@ -3,18 +3,39 @@ const doorImage2 = document.querySelector("#door2");
 const doorImage3 = document.querySelector("#door3");
 const botDoorPath = "https://content.codecademy.com/projects/chore-door/images/robot.svg";
 const beachDoorPath = "https://content.codecademy.com/projects/chore-door/images/beach.svg";
-const spaceDoorPath = "https://content.codecademy.com/projects/chore-door/images/space.svg"
+const spaceDoorPath = "https://content.codecademy.com/projects/chore-door/images/space.svg";
+const numClosedDoors = 3;
+let openDoor1, openDoor2, openDoor3;
 
+const randomChoreDoorGenerator = () => {
+    let choreDoor = Math.floor(Math.random() * numClosedDoors);
+    if (choreDoor === 0) {
+        openDoor1 = botDoorPath;
+        openDoor2 = beachDoorPath;
+        openDoor3 = spaceDoorPath;
+    }
+    else if (choreDoor === 1) {
+        openDoor2 = botDoorPath;
+        openDoor1 = spaceDoorPath;
+        openDoor3 = beachDoorPath;
+    }
+    else if (choreDoor === 2) {
+        openDoor3 = botDoorPath;
+        openDoor2 = beachDoorPath;
+        openDoor1 = spaceDoorPath;
+    }
+};
 
 doorImage1.onclick = event => {
-    event.target.src = botDoorPath;
+    event.target.src = openDoor1;
 }
 
 doorImage2.onclick = event => {
-    event.target.src = beachDoorPath;
+    event.target.src = openDoor2;
 }
 
 doorImage3.onclick = event => {
-    event.target.src = spaceDoorPath;
+    event.target.src = openDoor3;
 }
 
+randomChoreDoorGenerator();
